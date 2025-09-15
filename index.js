8421e192626823e611b3740bcd3b2952b7787f33
merialea <merialea5@gmail.com>
2025-09-15 12:05:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+const cats = ["Milo","Otis", "Garfield"]
+
+beforeEach(function (){
+    cats.length = 0,
+
+    cats.push("Milo","Otis", "Garfield");
+})
+
+function destructivelyAppendCat(){
+    cats .push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+} 
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+}
+
+
+function appendCat(){
+    let newArray = [...cats]
+    newArray.push("Broom")
+    return newArray
+}
+
+function prependCat(){
+    let newArray = [...cats]
+    newArray.unshift("Arnold")
+    return newArray
+}
+
+function removeLastCat(){
+    let newArray = [...cats]
+    newArray.pop("Garfield")
+    return newArray
+}
+
+function removeFirstCat(){
+    let newArray = [...cats]
+    newArray.shift("Milo")
+    return newArray
+}
+
+
+
+
